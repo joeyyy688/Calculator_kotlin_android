535f18d59143646a89440b63914dee8344b20631
joeyyy688 <edinjoey@gmail.com>
2023-04-05 01:53:09 +0000

    Added some few app functionalities

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
@@ -11,10 +12,14 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    private var calculatorInputTextView: TextView? = null
+    private var _calculatorInputTextView: TextView? = null
+    var isLastCharacterNumeric: Boolean = false
+    var isLastCharacterDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
         // Define ActionBar object
         // Define ActionBar object
@@ -34,12 +39,48 @@ class MainActivity : AppCompatActivity() {
         // Set BackgroundDrawable
         actionBar?.setBackgroundDrawable(colorDrawable)
 
-        calculatorInputTextView =  findViewById<TextView>(R.id.textViewInputField)
+        _calculatorInputTextView =  findViewById<TextView>(R.id.textViewInputField)
     }
 
     fun onDigit(view: View){
-        calculatorInputTextView?.append("2")
-        Toast.makeText(this, "Toast displayed", Toast.LENGTH_LONG ).show()
+
+        _calculatorInputTextView?.append((view as Button).text)
+
+        isLastCharacterNumeric = true
+        isLastCharacterDot = false
+
+        //Toast.makeText(this, "Toast displayed", Toast.LENGTH_LONG ).show()
     }
+
+    fun onClear(view: View){
+        _calculatorInputTextView?.text = ""
+    }
+
+    fun onDecimalPoint (view: View){
+        if (isLastCharacterNumeric && !isLastCharacterDot){
+            _calculatorInputTextView?.append(".")
+            isLastCharacterNumeric = false
+            isLastCharacterDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        _calculatorInputTextView?.text?.let {
+
+            if (isLastCharacterNumeric && !isOperatorAdded(it.toString()) ){
+                _calculatorInputTextView?.append((view as Button).text)
+
+                isLastCharacterNumeric = false
+                isLastCharacterDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if (value.startsWith("-") ){
+            true
+        }else value.contains("-") || value.contains("+") || value.contains("*") || value.contains("/")
+    }
+
 
 }
