3e727fa1883930918f661ec0b75b02ad912cddee
joeyyy688 <edinjoey@gmail.com>
2023-04-10 17:24:35 +0000

    Completed Calculator App

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private var _calculatorInputTextView: TextView? = null
     var isLastCharacterNumeric: Boolean = false
     var isLastCharacterDot: Boolean = false
+    var operatorCharacter: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,10 +71,61 @@ class MainActivity : AppCompatActivity() {
             if (isLastCharacterNumeric && !isOperatorAdded(it.toString()) && it.isNotEmpty()){
                 _calculatorInputTextView?.append((view as Button).text)
 
+                operatorCharacter = (view as Button).text as String?
+
                 isLastCharacterNumeric = false
                 isLastCharacterDot = false
             }
         }
+    }
+
+    fun onEqualOperator(view: View) {
+        if (isLastCharacterNumeric){
+            try {
+                val calValue =  _calculatorInputTextView?.text.toString()
+                val calculatorValues = calValue.split("$operatorCharacter")
+
+             if (calculatorValues.count() > 1){
+                 val firstNumber = calculatorValues[0]
+                 val lastNumber = calculatorValues[1]
+
+                 _calculatorInputTextView?.text = operatorCharacter?.let {
+                     removeZeroAfterDot(
+                         runCalculation(firstNumber.toDouble(), lastNumber.toDouble(), it).toString()
+                     )
+                 }
+             }
+            }catch (e: ArithmeticException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun runCalculation(firstNum: Double, secondNum: Double, operator: String): Double {
+        return when (operator) {
+            "+" -> {
+                (firstNum + secondNum)
+            }
+            "-" -> {
+                (firstNum - secondNum)
+            }
+            "*" -> {
+                (firstNum * secondNum)
+            }
+            else -> {
+                (firstNum / secondNum)
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String {
+        var value = result
+
+        if(value.contains(".0")){
+            value = result.substring(0, (result.length - 2))
+        }
+
+        return value
     }
 
     private fun isOperatorAdded(value: String) : Boolean {
